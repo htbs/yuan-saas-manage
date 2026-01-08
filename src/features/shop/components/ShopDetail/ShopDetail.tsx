385ad296f2369afa001a61991de0bd8e568b7f54
htb <2215129852@qq.com>
2026-01-08 19:43:38 +0800

    增加商铺详情

--- src/features/shop/components/ShopDetail/ShopDetail.tsx
+++ src/features/shop/components/ShopDetail/ShopDetail.tsx
@@ -1,1 +1,18 @@
 // 详情、修改、新增
+
+import { useShopStore } from "../../stores/useShopStore";
+
+interface ShopDetailProps {
+  id?: string;
+}
+
+export function ShopDetail(props: ShopDetailProps) {
+  // 详情ID
+  const shopId = useShopStore((state) => state.editId);
+  // 设置详情视图
+  const setView = useShopStore((state) => state.setView);
+  // 详情视图
+  const view = useShopStore((state) => state.view);
+
+  return <div>这里是店铺详情{shopId}</div>;
+}
